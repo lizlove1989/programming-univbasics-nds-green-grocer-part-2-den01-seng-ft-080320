3334efb1c6fd9c4b5e3bde40e68bde323b457452
Liz Love <love.elizabeth1989@gmail.com>
2020-07-21 20:48:24 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,15 +1,44 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  cart.each_with_index do |grocery, index|
+    groceryName = grocery[:item]
+    groceryCount = grocery[:count]
+    coupon = coupons[index]
+    couponName = coupon ? coupon[:item] : nil
+
+    countToApply = coupon ? coupon[:num] : nil
+    
+    if groceryName ==  couponName
+      if groceryCount > countToApply
+        remainder = 0
+        itWithDiscount = {}
+        
+        remainder = groceryCount % countToApply
+        countWithDiscount = groceryCount - remainder
+        
+        grocery[:count] = remainder
+        
+        itemWithDiscount = {
+          :item => "#{groceryName} W/COUPON",
+          :price => coupon[:cost] / countWithDiscount,
+          :clearance => "true",
+          :count => countWithDiscount
+        }
+        cart << itemWithDiscount
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  cart.each do |item, details|
+    if cart[item][:clearance] == true
+      cart[item][:price] = (cart[item][:price]*0.20 - total)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
